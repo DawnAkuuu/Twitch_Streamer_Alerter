8e3eca37846f17d33b12a276d793665557ff00ca
DawnAkuuu <luissilvas12411@gmail.com>
2025-08-12 01:41:23 -0700

    Added HTTP request to check for onlineness

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,14 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
+#define FEATUREPIN 14
 
 Audio audio;  // main audio object
 
+unsigned long lastCheck = 0;
+const unsigned long checkInterval = 600000; // 10 min
+
+bool isPlaying = false; 
 
 const char* ssid = "";
 const char* password = "";
@@ -18,6 +23,17 @@ void setup() {
     Serial.println("SPIFFS Mount Failed!");
     while (true);
   }
+  WiFi.begin(ssid, password);
+
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+    Serial.println(WiFi.status());
+  }
+  Serial.println("Connected to WiFi");
+  pinMode(FEATUREPIN, OUTPUT);
+
 
   // Init I2S for MAX98357A
   // Args: BCLK, LRC, DOUT
@@ -27,9 +43,55 @@ void setup() {
   audio.setVolume(21);  
 
   // Play from SPIFFS
-  audio.connecttoFS(SPIFFS, "/WAKE THE FUCK UP SONG.mp3");
+  // audio.connecttoFS(SPIFFS, "/WAKE THE FUCK UP SONG.mp3");
 }
 
 void loop() {
   audio.loop(); // must be called continuously
+
+
+  if (millis() - lastCheck > checkInterval) {
+    lastCheck = millis();
+    String payload = "";
+    if (WiFi.status() == WL_CONNECTED) {
+      HTTPClient http;
+      String serverPath = "https://decapi.me/twitch/uptime/soymilk"; // Replace with your target URL
+
+      http.begin(serverPath.c_str()); // Specify the URL
+
+      int httpResponseCode = http.GET(); // Send the GET request
+
+      if (httpResponseCode > 0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        payload = http.getString(); // Get the response payload
+        Serial.println(payload);
+      } else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+
+      http.end(); // Free resources
+    } else {
+      Serial.println("WiFi Disconnected");
+    }
+
+    if (payload == "soymilk is offline") {
+      Serial.println("Soymilk aint on");
+      digitalWrite(FEATUREPIN, LOW);
+    }
+    else {
+      Serial.println("Soymilk be on");
+      digitalWrite(FEATUREPIN, HIGH);
+      if (!isPlaying) {
+        audio.connecttoFS(SPIFFS, "/WAKE THE FUCK UP SONG.mp3");
+        isPlaying = true;
+        delay(200);
+      }
+      if (isPlaying && !audio.isRunning()) {  // playback finished
+        Serial.println("Playback finished");
+        isPlaying = false;
+      }
+    }
+  }
 }
